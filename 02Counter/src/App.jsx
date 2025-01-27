ad5a51a2d20c67ab27f09fbb9b4d8744d6d84901
ANSH KUMAR <anshkumar1399@gmail.com>
2025-01-28 00:18:24 +0530

    Counter useState

--- 02Counter/src/App.jsx
+++ 02Counter/src/App.jsx
@@ -1,10 +1,40 @@
 import { useState } from 'react'
+import './index.css'
 
 function App() {
 
+  let [counter, setcounter]=useState(0)
+  // let counter = 0
+  function addvalue() {
+    if (counter >= 0 && counter <20){
+      counter += 1;
+    }
+    else {
+      return counter = 0
+    }
+
+    console.log(setcounter(counter))
+  };
+  const decvalue = () => {
+    if (counter <= 0){
+      return counter = 0
+    }
+    else {
+      counter -= 1;
+    }
+
+    console.log(setcounter(counter))
+  };
   return (
     <>
-      <h1>Counter</h1>
+      <h1 className="text-3xl font-bold text-center">Counter</h1>
+      <div>
+        <h3 className="text-center text-xl mt-2">Counter:{counter}</h3>
+        <div className="flex flex-col items-center mt-5">
+          <button onClick={addvalue} className="cursor-pointer p-1 flex w-fit bg-amber-300">Add value</button>
+          <button onClick={decvalue} className="cursor-pointer w-fit mt-1 p-1 bg-blue-400">Dec value</button>
+        </div>
+      </div>
     </>
   )
 }
